9d0d00849d8c5ed28ead45f85bca59d71b1af129
Strahinja Lukić <strahinja.lukic.011@gmail.com>
2023-03-19 15:27:07 +0100

    Initial definition of deck state

--- tables-api/include/deck_state.h
+++ tables-api/include/deck_state.h
@@ -1,3 +1,34 @@
 #pragma once
 
 #include "card.h"
+
+#include <memory>
+#include <unordered_set>
+
+/**
+ * Class [DeckState] stores the state of the card deck at the given point in the game.
+ * The state of the deck is described by defining the sets of the cards at all relevant locations at
+ * the table: Cards held in each players hands, cards taken by each player, the talon, and the
+ * remaining cards to deal.
+ */
+class DeckState
+{
+public:
+    using CardSet = std::unordered_set<Card>;
+
+    DeckState();
+    ~DeckState();
+
+    /** Returns the set of cards held by the player with index player_idx in their hands. */
+    CardSet PlayerHand(std::uint32_t player_idx);
+    /** Returns the set of cards taken by the player with index player_idx. */
+    CardSet PlayerTaken(std::uint32_t player_idx);
+    /** Returns the set of cards laid down facing up on the table (the "talon"). */
+    CardSet Talon();
+    /** Returns the set of cards remaining to be dealt in the forthcoming rounds. */
+    CardSet Remaining();
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
+};
